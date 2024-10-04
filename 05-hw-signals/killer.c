a63af9bbab667606081dbebf5d01de8f6a4f0312
Mark Taylor Bradshaw <marktb@mainstay.cs.byu.edu>
2024-10-04 16:45:20 -0600

    hw 5

--- 05-hw-signals/killer.c
+++ 05-hw-signals/killer.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -28,26 +29,102 @@ int main(int argc, char *argv[]) {
 
 	switch (scenario[0]) {
 	case '0':
+          kill(pid, SIGHUP);
+          sleep(1);
 		break;
 	case '1':
+		// kill(pid, SIGPWR);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '2':
+		kill(pid, SIGUSR2);
+		kill(pid, SIGHUP);
+		sleep(6);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '3':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(5);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '4':
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGINT);
+		sleep(10);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '5':
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '6':
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 16);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '7':
+		kill(pid, SIGUSR2);
+		sleep(1);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '8':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGSYS);
+		sleep(1);
+		kill(pid, SIGUSR1);
+		sleep(1);
+		kill(pid, SIGPWR);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
-	case '9': 
+	case '9':
+		kill(pid, SIGSYS);
+		sleep(1);
+		kill(pid, SIGQUIT);
+		sleep(1);
+		kill(pid, SIGUSR2);
+		sleep(1);
+		kill(pid, SIGSYS);
+		sleep(5);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
-
 	}
 	waitpid(pid, NULL, 0);
 }
